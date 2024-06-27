3a78f3c8033767253628379dafa8439d9deea99f
Chris Bearden <123311504+cbear1984@users.noreply.github.com>
2024-06-27 06:59:45 -0500

    Initial Commit

---
+++ The Meal Challenge/The_Meal_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Meal_ChallengeApp.swift
+//  The Meal Challenge
+//
+//  Created by Chris Bearden on 6/27/24.
+//
+
+import SwiftUI
+
+@main
+struct The_Meal_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
